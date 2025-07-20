ea42ef557a4cff1c68b1ea5861c20a2f374428a4
Prathamesh <prathameshjangle425@gmail.com>
2025-07-21 00:41:40 +0530

    Time: 0 ms (100%), Space: 59.2 MB (91.11%) - LeetHub

--- 1046-max-consecutive-ones-iii/1046-max-consecutive-ones-iii.cpp
+++ 1046-max-consecutive-ones-iii/1046-max-consecutive-ones-iii.cpp
@@ -10,7 +10,7 @@ public:
         while(r < n){
             if(nums[r] == 0) count++;
 
-            while(count > k){
+            if(count > k){
                 if(nums[l] == 0){
                     count--;
                 }
